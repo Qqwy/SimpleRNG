538c7420485566535145d3311e4be89efe7a6ac6
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-09-15 16:10:29 +0200

    Updated Haskell example

--- haskell/SimpleRNG.hs
+++ haskell/SimpleRNG.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module SimpleRNG (
                  seed,
-                 rand_r
+                 random,
+                 randoms
                  )
 where
 
@@ -18,17 +19,15 @@ where
        Usage instructions:
 
        1. use `rng_state = SimpleRNG.seed your_lucky_number`  to seed the RNG to a desired 32-bit unsigned integer.
-       2. Call `SimpleRNG.rand rng_state` to get a new random 32-bit unsigned integer.
+       2. Call `(random_number, new_rng_state) =  SimpleRNG.rand rng_state` to get a new random 32-bit unsigned integer.
+       3. Repeat step (2) as often as you'd like.
 
       Because we're in a pure functional language, you'll keep track of the `rng_state` yourself somewhere.
-      For instance, to an infinite stream of random numbers:
+      It's very common to want an infinite stream of random numbers.
+      For this common pattern, the wrapper function `randoms` has been provided.
 
-      `infinite_stream lucky_number = map fst $ iterate (\(_, rng) -> rand_r rng) (rand_r $ seed lucky_number)`
 
-      NOTE: While this library implements RandomGen, theoretically making it possible to use `take 10 $ System.Random.randoms (SimpleRNG.seed 42)` or the likes,
-            this will not result in a deterministic range of values that is the same as the C, Java and Python implementations of this library;
-            I do not yet know why; track the progress of this question here:
-                  https://stackoverflow.com/questions/52343524/haskell-randomgen-drops-half-of-values
+      NOTE: While this library implements RandomGen, theoretically making it possible to use it instead of other built-in Haskell random generators, because of the conversion between potential randomness-result types, it is not possible to use it for gaining the same deterministic sequence of values of the implementations of this library in other languages; More information: https://stackoverflow.com/questions/52343524/haskell-randomgen-drops-half-of-values
 
 
       NOTE2: If you have trouble installing this library, run `cabal install random` first. Since Haskell 7.2.1, `System.Random` is no longer part of core, but extracted to the 'random' library.
@@ -49,8 +48,8 @@ newtype RNGState = RNGState Word32
 seed :: Integral a => a -> RNGState
 seed = RNGState . fromIntegral
 
-rand_r :: RNGState -> (Word32, RNGState)
-rand_r (RNGState num) = (res, RNGState res)
+random :: RNGState -> (Word32, RNGState)
+random (RNGState num) = (res, RNGState res)
   where
     res = num
       |> xorshift 13
@@ -59,8 +58,11 @@ rand_r (RNGState num) = (res, RNGState res)
     xorshift :: Int -> Word32 -> Word32
     xorshift amount x = x `xor` (shift x amount)
 
+randoms :: Integral a => a -> [Word32]
+randoms lucky_number = map fst $ iterate (\(_, rng) -> random rng) (random $ seed lucky_number)
+
 instance RandomGen RNGState where
-  next seed_state = (first fromIntegral) $ rand_r seed_state
+  next seed_state = (first fromIntegral) $ random seed_state
     where
   genRange seed_state = (fromEnum (minBound `asTypeOf` seed_state),
                 fromEnum (maxBound `asTypeOf` seed_state))
@@ -70,3 +72,4 @@ instance RandomGen RNGState where
       (_, seed_state') = next seed_state
       (_, inverted_seed_state') = next inverted_seed_state
       inverted_seed_state = RNGState (maxBound - num)
+
