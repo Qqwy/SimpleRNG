4ec5a68631ee085bb41b25f9e2e5b771eaca9fce
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-09-15 15:45:25 +0200

    More idiommatic Java

--- java_implementation/Example.java
+++ java_implementation/Example.java
@@ -1,7 +1,8 @@
 public class Example {
     public static void main(String[] args) {
+        SimpleRNG rng = new SimpleRNG(42);
         for(int x = 0; x < 10; ++x){
-            System.out.println(Long.toString(SimpleRNG.rand()));
+            System.out.println(Long.toString(rng.rand()));
         }
     }
 }
--- java_implementation/SimpleRNG.java
+++ java_implementation/SimpleRNG.java
@@ -11,42 +11,32 @@ public class SimpleRNG {
 
        Usage instructions:
 
-       1. use SimpleRNG.seed(your_lucky_number) to seed the RNG to a desired 32-bit unsigned integer.
-       2. Call SimpleRNG.rand() to get a new random 32-bit unsigned integer.
+       1. use `rng = new SimpleRNG(luckyNumber)` to seed the RNG to a desired 32-bit unsigned integer.
+       2. Call `rng.rand()` to get a new random 32-bit unsigned integer.
+       3. Repeat step (2) as often as you'd like.
 
-       If you're working in an environment where re-entrancy is important (like when multi-threading), or want to keep track of multiple RNGs side-by-side, use `SimpleRNG.rand_r(rng_state)` insted, which will take the passed argument as RNG state (and mutate it in place to become the new state).
+       Re-seeding can be done using `rng.seed(someOtherNumber)`
 
-       (Creating a rng_state is done by calling `new SimpleRNG.RNGState(your_lucky_number)`).
 
        Be warned that although the library returns `longs`, the RNG will only ever output values between 0 and 2^32.
     */
-    public static class RNGState {
-        private long data;
-        public RNGState(){
-            this(42);
-        }
-        public RNGState(long seed) {
-            this.data = seed;
-        }
+    private static long max_32bit = (long) 1 << 32;
+
+    public SimpleRNG(long seed) {
+        this.seed(seed);
     }
 
-    private static long max_32bit = (long) 1 << 32;
-    public static long rand_r(SimpleRNG.RNGState rng_state) {
-        long num = rng_state.data;
+    long rng_state;
+    public long rand() {
+        long num = this.rng_state;
         num ^= (num << 13) % max_32bit;
         num ^= (num >> 17) % max_32bit;
         num ^= (num << 5) % max_32bit;
-        rng_state.data = num;
+        this.rng_state = num;
         return num;
     }
 
-    private static RNGState global_rng_state = new RNGState(42);
-
-    public static long rand() {
-        return rand_r(global_rng_state);
-    }
-
-    public static void seed(long seed) {
-        global_rng_state.data = seed;
+    public void seed(long seed) {
+        this.rng_state = seed;
     }
 }
